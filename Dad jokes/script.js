fdc4b56698fbbd371ca33ff2dd312297b4bd2ccc
Ritik76450 <89569774+Ritik76450@users.noreply.github.com>
2022-09-02 22:01:28 +0530

    Add files via upload

---
+++ Dad jokes/script.js
@@ -0,0 +1,17 @@
+const joke=document.getElementById('joke');
+const jokebtn=document.getElementById('jokeBtn');
+getJokes();
+jokeBtn.addEventListener('click', getJokes);
+function getJokes(){
+    const config={
+        headers:{
+            "Accept":"application/json"
+        }
+    }
+    fetch('https://icanhazdadjoke.com/',config)
+    .then(res=>res.json())
+    .then(data=>{
+        joke.innerText=data.joke;
+    });
+
+}
